da689db627d741bc3c8c7cf37496c384eb453756
chenzhigang <979703986@qq.com>
2022-01-02 19:26:50 +0800

    subcode trigger stories

--- components/Trigger/Trigger.tsx
+++ components/Trigger/Trigger.tsx
@@ -1,23 +1,26 @@
 
 import React from 'react'
+import ReactDOM from 'react-dom'
 import classnames from 'classnames'
 import { createPrefixCls } from '../utils/create'
 import Portal from '../Portal'
 import Align, { AlignType } from '../Align'
 
-type ActionType = 'click' | 'hover' | 'focus'
+type ActionType = 'click' | 'hover' | 'focus' | 'blur'
 
-export interface TriggerProps extends AlignType {
+export interface TriggerProps {
   className?: string
-  children: React.ReactNode
-  action: ActionType | ActionType []
+  children: React.ReactElement []
+  action?: ActionType | ActionType []
+  showAction?: ActionType[]
+  hideAction?: ActionType[]
+  align?: AlignType
 }
 
 const Trigger: React.FC<TriggerProps> = ({
   className,
   children,
-  points = ['tl', 'bl'],
-  offset = [0, 0],
+  align = { points: ['tl', 'bl'], offset: [0, 0] },
   ...restProps
 }) => {
 
@@ -25,15 +28,58 @@ const Trigger: React.FC<TriggerProps> = ({
     return null
   }
 
+  let [trigger, ...alignChildren] = React.Children.toArray(children)
+  const refTrigger = React.useRef()
+  const [visible, setVisible] = React.useState<boolean>(false)
+
   const cls = createPrefixCls('trigger')
   const classes = classnames(
     `${cls}`,
     className,
   )
 
+  const getContainer = () => {
+    const container = window.document.createElement('div')
+    Object.assign(container.style, {
+      position: 'absolute',
+      top: '0',
+      let: '0',
+      width: `100%`,
+    })
+    return container
+  }
+
+  const handleClick = () => {
+    setVisible(v => !v)
+    console.log('11')
+  }
+  const cloneProps = {
+    ref: refTrigger,
+    onClick: handleClick
+  }
+  trigger = React.cloneElement(trigger as React.ReactElement, cloneProps)
+
+  let portal: React.ReactElement | null = null
+  if (visible) {
+    portal = (
+      <Portal
+        key="portal"
+        getContainer={getContainer}
+      >
+        <Align
+          align={align}
+          target={() => refTrigger.current!}
+        >
+          {alignChildren[0] as React.ReactElement}
+        </Align>
+      </Portal>
+    )
+  }
+
   return (
     <>
-
+      {trigger}
+      {portal}
     </>
   )
 }
--- components/Trigger/index.stories.tsx
+++ components/Trigger/index.stories.tsx
@@ -1,5 +1,5 @@
 
-import React, {useRef} from 'react'
+import React from 'react'
 import { Story, Meta } from '@storybook/react/types-6-0'
 import Trigger, { TriggerProps } from './index'
 
@@ -11,29 +11,11 @@ export default {
 
 const Template: Story<TriggerProps> = args => {
 
-  const [visible, setVisible] = React.useState<boolean>(false)
-  const targetNode: any = React.useRef<HTMLDivElement | null>(null)
-  const childRef: any = useRef()
-
-  const changeInput = ({ target } : any) => {
-    childRef.current.open()
-  }
-
-  const alignProp = { visible, targetNode, childRef }
-
-  console.log('alignProp => ', alignProp)
-
   return (
-    <div ref={targetNode}>
-      <input
-        onChange={changeInput}
-        onFocus={() => {
-          if (visible) childRef.current.open()
-          else setVisible(true)
-        }}
-      />
-      <Trigger {...alignProp}>
-        <div>1231232131</div>
+    <div>
+      <Trigger>
+        <input/>
+        <p>12312312321313123</p>
       </Trigger>
     </div>
   )
