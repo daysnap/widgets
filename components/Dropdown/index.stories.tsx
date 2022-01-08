dd5cfa7bcd192b69a2a4e19d7032c589936fcb75
chenzhigang <979703986@qq.com>
2022-01-08 22:08:14 +0800

    subcode

--- components/Dropdown/index.stories.tsx
+++ components/Dropdown/index.stories.tsx
@@ -2,33 +2,37 @@
 import React from 'react'
 import { Story, Meta } from '@storybook/react/types-6-0'
 import Dropdown, { DropdownProps } from './index'
+import Placements from './placements'
 import Button from '../Button'
 
 export default {
   title: 'Dropdown',
   component: Dropdown,
-  argTypes: {},
+  argTypes: {
+    placement: {
+      control: { type: 'select' },
+      options: Object.keys(Placements),
+    }
+  },
 } as Meta
 
 const Template: Story<DropdownProps> = args => {
   return (
     <div>
-      <p>123123123123123</p>
-      <Dropdown trigger="click">
-        <button>菜单</button>
+      <Dropdown trigger="click" placement="topLeft">
+        <button>菜单topLeft</button>
         <Dropdown.Item disabled>帮助中心</Dropdown.Item>
         <Dropdown.Item>关于我们</Dropdown.Item>
         <Dropdown.Divider/>
         <Dropdown.Item>用户反馈</Dropdown.Item>
       </Dropdown>
-      <p>123123123123123</p>
-      <p>123123123123123</p>
-      <p>123123123123123</p>
     </div>
   )
 }
 
 export const Basic = Template.bind({})
 Basic.storyName = '基础用法'
-Basic.args = {}
+Basic.args = {
+  placement: 'topLeft'
+}
 
