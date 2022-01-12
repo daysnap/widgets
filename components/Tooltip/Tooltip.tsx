c539d4049084c1d8ade7dcd7f0a848509d699333
chenzhigang <979703986@qq.com>
2022-01-12 22:45:07 +0800

    tooltip

--- components/Tooltip/Tooltip.tsx
+++ components/Tooltip/Tooltip.tsx
@@ -2,13 +2,21 @@
 import React from 'react'
 import classnames from 'classnames'
 import { createPrefixCls } from '../utils/create'
+import Trigger from '../Trigger'
+import Placements from './placements'
 
 export interface TooltipProps {
   className?: string
+  placement?: 'bottomLeft' | 'bottomCenter' | 'bottomRight' | 'topLeft' | 'topCenter' | 'topRight'
+  children?: React.ReactNode
+  title?: string
 }
 
 const Tooltip: React.FC<TooltipProps> = ({
   className,
+  placement= 'bottomLeft',
+  children,
+  title,
   ...restProps
 }) => {
 
@@ -17,12 +25,24 @@ const Tooltip: React.FC<TooltipProps> = ({
     `${cls}`,
     className,
   )
+  const overlay = (
+    <span
+      className={classes}>
+      {title}
+    </span>
+  )
 
   return (
-    <div
-      {...restProps}
+    <Trigger
+      prefixCls={cls}
       className={classes}
-    />
+      align={Placements[placement]}
+      action="hover"
+      placements={Placements}
+    >
+      {children}
+      {overlay}
+    </Trigger>
   )
 }
 
--- components/Tooltip/index.stories.tsx
+++ components/Tooltip/index.stories.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import { Story, Meta } from '@storybook/react/types-6-0'
 import Tooltip, { TooltipProps } from './index'
+import Button from '../Button'
 
 export default {
   title: 'Tooltip',
@@ -9,9 +10,22 @@ export default {
   argTypes: {},
 } as Meta
 
-const Template: Story<TooltipProps> = args => <Tooltip {...args} />
+const Template: Story<TooltipProps> = args => {
+  return (
+    <dl>
+      <dt>基础用法</dt>
+      <dd>
+        <Tooltip {...args}>
+          <Button>按钮</Button>
+        </Tooltip>
+      </dd>
+    </dl>
+  )
+}
 
 export const Basic = Template.bind({})
 Basic.storyName = '基础用法'
-Basic.args = {}
+Basic.args = {
+  title: '点击按钮'
+}
 
