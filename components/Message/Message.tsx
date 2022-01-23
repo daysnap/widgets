4589ac06dbd712862adfaa1137a023f2704314ba
chenzhigang <979703986@qq.com>
2022-01-23 13:43:45 +0800

    message

--- components/Message/Message.tsx
+++ components/Message/Message.tsx
@@ -2,15 +2,31 @@
 import React from 'react'
 import classnames from 'classnames'
 import { createPrefixCls } from '../utils/create'
+import Icon from '../Icon'
+import { createPortal } from 'react-dom'
 
 export interface MessageProps {
   className?: string
 }
 
-const Message: React.FC<MessageProps> = ({
+export interface MessageType {
+  id?: number
+  delayed?: number
+  type?: string
+  content?: string
+  timer?: number
+}
+
+export interface MessageRef {
+  add: (message: MessageType) => void
+  remove: (id: number) => void
+}
+
+const Message: React.FC<MessageProps> = React.forwardRef<MessageRef, MessageProps>(({
   className,
-  ...restProps
-}) => {
+}, ref) => {
+
+  const [messages, setMessages] = React.useState<MessageType[]>([])
 
   const cls = createPrefixCls('message')
   const classes = classnames(
@@ -18,13 +34,46 @@ const Message: React.FC<MessageProps> = ({
     className,
   )
 
+  const add = (message: MessageType) => {
+    message = Object.assign({ id: Date.now(), delayed: 3000 }, message)
+    if (message.delayed! > 0) {
+      message.timer = setTimeout(remove, message.delayed, message.id)
+    }
+    setMessages([...messages, message])
+  }
+  const remove = (id: number) => {
+    const item = messages.find(item => item.id === id)
+    clearTimeout(item?.timer)
+    setMessages(messages.filter(item => item.id === id))
+  }
+  React.useImperativeHandle(ref, () => ({ add, remove }))
+
+  const children = messages.map((item, index) => {
+    const { content, type } = item
+    return (
+      <li style={{top: `${20 * (index + 1)}px`}}>
+        <Icon icon={`icon-${type}`}/>
+        <span>{content}</span>
+      </li>
+    )
+  })
+
   return (
-    <div
-      {...restProps}
-      className={classes}
-    />
+    <ul className={classes}>
+      {children}
+    </ul>
   )
+})
+
+let instance: MessageRef | null
+function createInstance () {
+  if (!instance) {
+    const instance = React.useRef<any>()
+    createPortal(<Message ref={instance}/>, document.body)
+  }
+  return instance
 }
+
 
 export default Message
 
