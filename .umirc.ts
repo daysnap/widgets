52785c4e92f2376b0e34bfa7660d0c10feafd43c
chenzhigang <979703986@qq.com>
2022-02-09 13:20:00 +0800

    [skip ci]

--- .umirc.ts
+++ .umirc.ts
@@ -50,6 +50,6 @@ export default defineConfig({
   locales: [['zh-CN', '中文'], ['en-US', 'English']],
   exportStatic: {}, // 后续会部署到 github pages 直接全部生成静态页面 不走前端路由
   dynamicImport: {}, // 拆包 站点过大时可以优化首屏加载速度
-  webpack5: {},
+  // webpack5: {},
   mfsu: {},
 })
