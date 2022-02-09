23e924218352eca1cdcc57b061ad0e76fec45aec
chenzhigang <979703986@qq.com>
2022-02-09 16:40:14 +0800

    subcode

--- .umirc.ts
+++ .umirc.ts
@@ -1,4 +1,6 @@
 
+// config https://d.umijs.org/zh-CN/config
+
 import path from 'path'
 import { defineConfig } from 'dumi'
 import pkg from './package.json'
@@ -12,7 +14,6 @@ if (process.env.SITE_BUILD_ENV === 'PREVIEW') {
   publicPath = undefined;
 }
 
-console.log('dd => ', path.join(__dirname, './components'))
 export default defineConfig({
   title: 'DaySnap Widgets',
   // favicon: '',
@@ -22,35 +23,27 @@ export default defineConfig({
   base,
   publicPath,
   resolve: {
-    includes: ['docs', 'src']
+    includes: ['docs', 'components']
   },
   alias: {
-    // [pkg.name]: path.join(__dirname, './components'),
-    'daysnap-widgets/lib': path.join(__dirname, './src'),
+    [pkg.name]: path.join(__dirname, './components'),
   },
   extraBabelPlugins:  [
     [
       'import',
       {
         libraryName: pkg.name,
-        libraryDirectory: 'lib',
-        // camel2DashComponentName: false,
+        libraryDirectory: '.',
         customStyleName: () => {
           return `../style/index.scss`; // 注意：这里 ./ 不可省略
         },
       },
-      'daysnap-widgets',
+      pkg.name,
     ],
   ],
-  sass: {
-    // 默认值 Dart Sass，如果要改用 Node Sass，可安装 node-sass 依赖，然后使用该配置项
-    // implementation: require('node-sass'),
-    // 传递给 Dart Sass 或 Node Sass 的配置项，可以是一个 Function
-    // sassOptions: {},
-  },
   locales: [['zh-CN', '中文'], ['en-US', 'English']],
   exportStatic: {}, // 后续会部署到 github pages 直接全部生成静态页面 不走前端路由
   dynamicImport: {}, // 拆包 站点过大时可以优化首屏加载速度
-  // webpack5: {},
+  webpack5: {},
   mfsu: {},
 })
