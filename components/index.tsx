cbb0a6bf0d865f19e6758799b66d302bb71b1d14
chenzhigang <979703986@qq.com>
2022-02-11 13:31:38 +0800

    subcode

--- components/index.tsx
+++ components/index.tsx
@@ -1,9 +1,6 @@
 
 /* 本文件自动生成 './bin/entry.js' */
 
-export type { ButtonProps } from './Button'
-export { default as Button } from './Button'
-
 export type { IconProps } from './Icon'
 export { default as Icon } from './Icon'
 
